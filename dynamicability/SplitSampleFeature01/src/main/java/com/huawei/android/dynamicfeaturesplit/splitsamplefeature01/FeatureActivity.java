b0c0d65e215c0a6290e6e81f148cd9594537ad6d
mayism <lican22@huawei.com>
2021-06-17 11:16:08 +0800

    dynamicability/update

--- dynamicability/SplitSampleFeature01/src/main/java/com/huawei/android/dynamicfeaturesplit/splitsamplefeature01/FeatureActivity.java
+++ dynamicability/SplitSampleFeature01/src/main/java/com/huawei/android/dynamicfeaturesplit/splitsamplefeature01/FeatureActivity.java
@@ -1,5 +1,17 @@
 /*
  * Copyright (c) Huawei Technologies Co., Ltd. 2012-2019. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.huawei.android.dynamicfeaturesplit.splitsamplefeature01;
@@ -39,7 +51,7 @@ public class FeatureActivity extends Activity {
         setContentView(R.layout.activity_feature);
         ImageView mImageView = findViewById(R.id.iv_load_png);
         mImageView.setImageDrawable(getResources().getDrawable(R.mipmap.activity));
-        Toast.makeText(this, "from Feature 01: " + stringFromJNI(), Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "from Feature 01: " + stringFromJNI() + " apple is: " + getResources().getString(R.string.apple), Toast.LENGTH_LONG).show();
     }
 
     /**
